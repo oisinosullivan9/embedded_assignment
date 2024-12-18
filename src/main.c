617e1669b12dfa8f1023239cb4082f2d0092ee2b
oisinosullivan9 <oisinosull@gmail.com>
2024-12-18 21:10:15 +0000

    Update collector IP address in main.c

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 #define LED_GPIO GPIO_NUM_2
 #define WIFI_SSID "AndroidAP7681"
 #define WIFI_PASSWORD "87654321"
-#define COLLECTOR_IP "192.168.197.61"  //change to your IP
+#define COLLECTOR_IP "192.168.251.61"  //change to your IP
 #define COLLECTOR_PORT 12345
 
 // Task Priorities 
